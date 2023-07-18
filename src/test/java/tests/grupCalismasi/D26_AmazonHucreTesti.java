98b819530fbd72c1641d3a3d6906a0e01586aee1
Akif GÜrkan <akifgurkan@hotmail.com>
2023-07-19 00:15:43 +0300

    day 15/2

---
+++ src/test/java/tests/grupCalismasi/D26_AmazonHucreTesti.java
@@ -0,0 +1,4 @@
+package tests.grupCalismasi;
+
+public class D26_AmazonHucreTesti {
+}
---
+++ src/test/java/tests/grupCalismasi/Soru_2.java
@@ -0,0 +1,25 @@
+package tests.grupCalismasi;
+
+import org.junit.Test;
+import utilities.ReusableMethods;
+import utilities.TestBaseCross;
+
+public class Soru_2  {
+
+    @Test
+    public void test01() {
+
+   // 1. AmazonPage sayfasinda istedigim satir ve sutun sayisi ile cagirdigimda bana hucredeki
+        // yaziyi getirecek bir method olusturun
+
+
+   // 2. Tests paketi altinda yeni bir class olusturun: D26_AmazonHucreTesti
+
+   // 3. Bu class’in altinda bir test method olusturun : hucretesti() ve webtable’da 3. satir.2.sutundaki
+        // yazinin “Home Services” yazisi icerdigini test edin
+
+   // 4. Yeni bir method olusturun : AmazonYazisi() ve tabloda 9 Hucrede “Amazon” yazisi
+   // bulundugunu test edin
+
+    }
+}
